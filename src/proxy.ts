fe5e70c1ac65cf0d5c1be14d93faadf801f19686
Richard10454 <richard2512006@gmail.com>
2026-06-19 23:54:08 -0400

    feat: renombrar la función de middleware a proxy para mayor claridad

--- src/proxy.ts
+++ src/proxy.ts
@@ -31,7 +31,7 @@ function isExpired(token: string | undefined): boolean {
   }
 }
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl
 
   const accessToken  = req.cookies.get(COOKIE_ACCESS)?.value
